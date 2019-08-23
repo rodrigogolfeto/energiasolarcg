bb28c648c2a17f0fd33dc5602c940e39899c26c9
Rodrigo Golfeto <rgolfeto@gmail.com>
2019-08-22 22:38:12 -0400

    Páginas 1 e 2 completas

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/css.css">
+    <title>Energia Solar CG</title>
+</head>
+
+<body>
+    <div class="container pt-4">
+
+        <img src="images/capa.png" alt="Energia Solar CG" class="w-100 mt-2" />
+
+        <div class="row d-flex justify-content-between">
+            <div class="col px-5 py-4">
+                <div class="font-28 font-italic color-91b122">PROPOSTA</div>
+                <div class="font-24 color-1d5033 line-height-140 mb-3">
+                    <span class="font-weight-bold">Jenifer do Tinder</span><br />
+                    Presidente Prudente - SP<br />
+                    3,6 kWp<br />
+                    <span class="color-91b122 font-18">18 99718 0371</span><br>
+                    <span class="font-italic color-1d5033 font-14">Válida até: 17/08/19</span>
+                </div>
+            </div>
+            <div class="col px-5 py-4 text-right color-fe3b00 font-20 line-height-140">
+                <span class="font-weight-bold">Cotação: 1120</span><br />
+                Unidade: Campo Grande<br />
+                Vendedor: Ney<br><br><br>
+                <span class="border-signature w-50 d-inline-block mt-5 color-1d5033 font-italic font-14 font-weight-bold">De acordo</span>
+            </div>
+        </div>
+
+        <div class="quebrar-pagina"></div>
+
+        <div class="text-center pt-5">
+            <img src="images/logo-solar.png" alt="Logo Solar" class="py-4 mb-4 mt-4" />
+            <p class="color-1d5037 font-24 py-3 mb-5 pb-5">Oferecemos soluções adequadas ao perfil de cada cliente.<br />Tudo é analisado da maneira mais minuciosa possível, e<br />isso para garantir que a sua unidade geradora atenda<br />todas as suas necessidades.</p>
+            <h3 class="color-fe3b00 py-5 d-inline-block mt-4">DIFERENCIAIS</h3>
+            <p class="color-1d5037 font-24 font-weight-bold line-height-130 mb-5 d-block pb-5">Somos correspondente de Crédito Direto ao Consumidor,<br>por isso realizamos todo o trâmite financeiro em nosso escritório<br>para que tudo seja facilitado para você.<br><br>O seu financiamento contempla 100% do seu sistema<br>gerador, incluindo aquisição dos equipamentos e instalação.<br><br>Carência de 60 dias<br>Juros a partir de 0,99% a.m.<br>Até 60 meses para pagar<br>Financiamento para Pessoa Física e Jurídica<br><br>Disponibilizamos seguro contra explosões, incêndio,<br>vendaval, tempestades elétricas, roubo e furto de<br>100% do valor dos equipamentos pra todos os nossos<br>projetos, tanto residenciais como comerciais.<br><br>Possuímos registro ativo no CREA<br /><br /><br /><br /></p>
+            <div class="text-center text-white bg-color-fe3b00 w-100 py-3 font-20">www.energiasolarpp.com.br</div>
+        </div>
+
+        <div class="quebrar-pagina"></div>
+
+        <div class="text-center pt-5">
+            <h3 class="color-fe3b00 pt-5 pb-4 d-inline-block mt-4">NOSSAS INSTALAÇÕES</h3>
+            <p class="color-1d5037 font-24 font-weight-bold line-height-130 mb-5 d-block pb-4">Possuimos mais de 500 projetos homologados e estes são apenas alguns deles:</p>
+            <div class="px-5 color-fe3b00 font-weight-bold font-16 pt-2 pb-5 mb-4">
+                <div class="row d-flex justify-content-center px-5">
+                    <div class="col-4 text-center p-0"><img src="images/imagem-1.png" alt="" class="w-100" /></div>
+                    <div class="col-4 text-center p-0"><img src="images/imagem-2.png" alt="" class="w-100" /></div>
+                    <div class="col-12 text-center py-2">Presidente Prudente SP</div>
+                </div>
+                <div class="row d-flex justify-content-center px-5">
+                    <div class="col-4 text-center p-0">
+                        <img src="images/imagem-3.png" alt="" class="w-100" />
+                        <p class="py-2 mb-0">Umuarama PR</p>
+                    </div>
+                    <div class="col-4 text-center p-0">
+                        <img src="images/imagem-4.png" alt="" class="w-100" />
+                        <p class="py-2 mb-0">Mirandópolis SP</p>
+                    </div>
+                </div>
+                <div class="row d-flex justify-content-center px-5">
+                    <div class="col-4 text-center p-0">
+                        <img src="images/imagem-5.png" alt="" class="w-100" />
+                        <p class="py-2 mb-0">Regente Feijó SP</p>
+                    </div>
+                    <div class="col-4 text-center p-0">
+                        <img src="images/imagem-6.png" alt="" class="w-100" />
+                        <p class="py-2 mb-0">Junqueirópolis SP</p>
+                    </div>
+                </div>
+                <div class="row d-flex justify-content-center px-5">
+                    <div class="col-4 text-center p-0">
+                        <img src="images/imagem-7.png" alt="" class="w-100" />
+                        <p class="py-2 mb-0">Lucélia SP</p>
+                    </div>
+                    <div class="col-4 text-center p-0">
+                        <img src="images/imagem-8.png" alt="" class="w-100" />
+                        <p class="py-2 mb-0">Pres. Epitácio SP</p>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center text-white bg-color-fe3b00 w-100 py-3 font-20 mt-5 d-inline-block">www.energiasolarpp.com.br</div>
+        </div>
+
+
+        <div class="quebrar-pagina"></div>
+
+        <div class="text-center">
+            <h3 class="color-fe3b00 pt-5 pb-4 d-inline-block mt-4">SOLUÇÃO PROPOSTA</h3>
+
+
+            <h4>EQUIPAMENTOS</h4>
+
+            <br><br><br><br>
+
+            Inversor solar:
+            1 Fronius Primo 4.0-1 4kW 220V
+            Módulos fotovoltaicos:
+            9 Trina Solar TSM-DE15M II Monocristalino 400W
+            Área necessária:
+            18 m²
+            Estimativa de geração mensal:
+            450 kWh/mês*
+            * Alterável em função do clima
+
+
+            Valor à vista: Entrada: Taxa de juros
+            a partir de 0,99% a.m.
+            6 parcelas 12 parcelas 24 parcelas
+            sem juros
+            36 parcelas 48 parcelas 60 parcelas
+            IMPORTANTE:
+            O valor cobre a compra e a instalação da unidade geradora
+            Adequações estruturais são de responsabilidade do proprietário
+            A taxa de juros e o parcelamento podem variar de acordo com o score do cliente
+            Os equipamentos podem ser substituídos sem aviso prévio quando não há exigência de marca
+
+            R$ 771,20
+            R$ 430,97 R$ 363,87
+            R$ 2.733,73 R$ 1.456,41
+            R$ 543,85
+            R$ 21.402,37 R$ 5.000,00
+
+
+        </div>
+
+
+    </div>
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
